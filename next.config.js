b7b1b11a7b7c53b59961a0728b4940b282c15e68
unknown <alpha1.abdullah@gmail.com>
2025-12-31 06:23:34 +0200

    first commit

--- next.config.js
+++ next.config.js
@@ -4,10 +4,15 @@ const nextConfig = {
         return [{
             source: '/(.*)',
             headers: [{
-                key: 'Content-Security-Policy',
-                value: "frame-ancestors https://ll-ms-attentions-arabic-explanation-ebon.vercel.app",
-            }, ],
-        }, ];
+                    key: 'Content-Security-Policy',
+                    value: "frame-ancestors https://ll-ms-attentions-arabic-explanation-ebon.vercel.app",
+                },
+                {
+                    key: 'Access-Control-Allow-Origin',
+                    value: "https://ll-ms-attentions-arabic-explanation-ebon.vercel.app",
+                },
+            ],
+        }];
     },
 };
 
